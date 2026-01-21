80b078645867db232f144b9ba073f0e349819631
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-01-21 10:46:02 +0530

    header

--- http.js
+++ http.js
@@ -1,9 +1,11 @@
 let http=require("http");
 let server = http.createServer((req,res)=>{
+  console.log(req.abc);
+  console.log(res.abc);
   res.write("Hello http module in Node.js !");
   res.end();
-}).listen(3000);
+});
 
-// server.listen(3000,()=>{
-//     console.log("Server is listening on port 3000");
-// });
+server.listen(3000,()=>{
+    console.log("Server is listening on port 3000");
+});
