a85dc05c8bad8685061900c10b2c97b3643cd36b
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-01-28 10:50:42 +0530

    ejs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+
+
+app.set('view engine','ejs');
+
 app.get('/',(req,res)=>{
-    res.send(index);
+    res.render('index');
 });
 
 app.get('/login',(req,res)=>{
