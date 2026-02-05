a072f45d710ca37d01027a3594b8bdf719892eb3
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-02-05 13:27:21 +0530

    revision

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let math = require('./rev.js');
+// let math = require('./rev.js');
 
-console.log("Sum: " + math.sum(5, 10));
-console.log("Product: " + math.product(5, 10));
+// console.log("Sum: " + math.sum(5, 10));
+// console.log("Product: " + math.product(5, 10));
+console.log("Hello World!");
 
