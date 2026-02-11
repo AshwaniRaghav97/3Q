6e29682d53ce5a74591b745029c10f4a2f3059e4
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-02-11 10:41:44 +0530

    create server in node js

--- http.js
+++ http.js
@@ -1,11 +1,17 @@
 let http=require("http");
 let server = http.createServer((req,res)=>{
-  // res.statusCode = 204;
-  res.statusCode = 201;
   res.write("Hello http module in Node.js !");
   res.end();
 });
 
 server.listen(3000,()=>{
-    console.log("Server is listening on port 3000");
+    console.log("Server is listening on port 3000"); 
 });
+
+
+
+
+
+
+
+
