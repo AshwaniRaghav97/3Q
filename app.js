05afdf251474759bcf29324077f6e2713d8307cc
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-02-13 13:10:49 +0530

    useer input

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-let math = require('./rev.js');
-
-console.log("Sum: " + math.sum(5, 10));
-console.log("Product: " + math.product(5, 10));
-console.log("Hello World!");
+console.log(process.argv);
 
 
