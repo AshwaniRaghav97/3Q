74545fa16b616a2e3044aded36c38725abfb648a
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-02-18 10:05:33 +0530

    url

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-console.log(process.argv);
+let {url} = require('url');
+let url = new URL('https://www.example.com/path?name=abc&age=30');
+
+console.log(url.protocol);
+console.log(url.host);
+console.log(url.pathname);
+console.log(url.searchParams.get('name'));
+console.log(url.searchParams.get('age'));
+console.log(url.href);
+
 
 
 
