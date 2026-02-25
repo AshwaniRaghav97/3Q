53a5c2a4526b95da6e3d280faacb0b328a848ba1
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-02-25 20:55:20 +0530

    comment

--- crud.js
+++ crud.js
@@ -12,6 +12,6 @@ let server = http.createServer((req,res)=>{
   res.end();
 });
 
-server.listen(3000,()=>{
-    console.log("Server is listening on port 3000"); 
-});
+// server.listen(3000,()=>{
+//     console.log("Server is listening on port 3000"); 
+// });
