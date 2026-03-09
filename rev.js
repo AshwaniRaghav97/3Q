d30e6d7b069add9349afb9f561bc3856deeef100
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-03-09 21:53:28 +0530

    commit

--- rev.js
+++ rev.js
@@ -7,6 +7,8 @@ let server = http.createServer((req, res) => {
         if(error) throw error;
         res.end(data);
     })
-})
+}).listen(3000,()=>{
+    console.log("Server is running on port 3000");
+});
 
 
