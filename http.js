c6861bce67c81a7ad639a6d173a7e77f16ff10e8
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-04-10 08:35:45 +0530

    commited

--- http.js
+++ http.js
@@ -1,11 +1,11 @@
 let http=require("http");
 let server = http.createServer((req,res)=>{
-  res.write("Hello World");
+  res.write("Hello World from Node.js HTTP Server!");
   res.end();
 });
 
 server.listen(3000,()=>{
-    console.log("Server is listening on port 3000"); 
+    console.log("Server is listening on port 3000");
 });
 
 
